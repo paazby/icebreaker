e008100124b7a91439aa96e2adcd0b560c511e5f
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-07-09 15:18:18 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,139 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      prod: {
+        src:[
+          'public/lib/**/*.js',
+          'public/client/*.js'
+        ],
+        dest: 'public/dist/minified.js'
+      }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+
+    uglify: {
+      prod:{
+        files:{
+          'public/dist/miniugly.js': 'public/dist/minified.js',
+        }
+      }
+    },
+
+    jshint: {
+      files: [
+        // Add filespec list here
+      ],
+      options: {
+        force: 'true',
+        jshintrc: '.jshintrc',
+        ignores: [
+          'public/lib/**/*.js',
+          'public/dist/**/*.js'
+        ]
+      }
+    },
+
+    cssmin: {
+      prod:{
+        src: ['public/style.css'],
+        dest:'public/dist/style.min.css'
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'public/client/**/*.js',
+          'public/lib/**/*.js',
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      },
+      css: {
+        files: 'public/*.css',
+        tasks: ['cssmin']
+      }
+    },
+
+    shell: {
+      prodServer: {
+        command: 'git push azure master'
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('default',[]);
+
+  grunt.registerTask('test', [
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('build', [
+    'concat',
+    'uglify',
+    'cssmin',
+    'jshint'
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    if(grunt.option('prod')) {
+      // add your production server task here
+      grunt.task.run(['shell']);
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    // add your deploy tasks here
+    'build',
+    'test',
+    'upload'
+  ]);
+
+
+};
