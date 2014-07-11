d9bec160289843ada788b12bf4c600f0e43dd3be
Albrey Brown <albrey.brown@gmail.com>
2014-07-11 16:52:10 -0700

    Changed pwd to __Dirname

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
-var app = require('./server-config.js');
+var express = require('express');
+var app = express();
 
-var port = process.env.PORT || 4568;
+app.configure(function() {
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser('shhhh, very secret'));
+  app.use(express.bodyParser());
+  app.use(express.session({secret: 'keyboard cat'}));
+});
+
+
+
+app.get('/matches',  function(req,res){
+  console.log('triggered');
+  res.status(200).sendfile(__dirname + '/mockData/test.txt');
+});
+
+
+app.get('/events', function(req, res){
+  res.status(200).sendfile(__dirname + '/mockData/test.txt');
+}); 
+  
+//   // fs.readFile('pathToFile', function(err, content){
+//   //   manipulate content to json
+//   //   stringify
+//   //   then send? 
+
+//   // })
+
+
+
+var port = process.env.PORT || 8080;
 
 app.listen(port);
 
