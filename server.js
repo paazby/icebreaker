65a155bc9ff8fc10f928312a71500856b0352458
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-07-11 20:00:41 -0700

    allow CORS on server

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', config.allowedDomains);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
 app.configure(function() {
   app.use(express.static(__dirname + '/public'));
   app.use(express.cookieParser('shhhh, very secret'));
   app.use(express.bodyParser());
+  app.use(allowCrossDomain);
   app.use(express.session({secret: 'keyboard cat'}));
 });
+
+
 
 
 
