8c61317522d3ab4b560b69ab25eabf4db8d0ec48
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-07-14 13:57:05 -0700

    delete mock data

--- server.js
+++ server.js
@@ -1,49 +1,6 @@
-var express = require('express');
-var app = express();
+var app = require('./server-config.js');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.allowedDomains);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-};
-
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.cookieParser('shhhh, very secret'));
-  app.use(express.bodyParser());
-  app.use(allowCrossDomain);
-  app.use(express.session({secret: 'keyboard cat'}));
-});
-
-
-
-
-
-app.get('/matches',  function(req,res){
-  console.log('triggered');
-  res.status(200).sendfile(process.cwd() + '/mockData/test.txt');
-});
-
-app.get('/',  function(req,res){
-  res.status(200).sendfile(process.cwd() + '/public/client/www/index.html');
-});
-
-app.get('/events', function(req, res){
-  res.status(200).sendfile(process.cwd() + '/mockData/test.txt');
-});
-
-//   // fs.readFile('pathToFile', function(err, content){
-//   //   manipulate content to json
-//   //   stringify
-//   //   then send?
-
-//   // })
-
-
-
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 4568;
 
 app.listen(port);
 
