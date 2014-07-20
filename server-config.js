1dcf10b2e11af43016426c525b4f8d6d037819c7
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-07-20 16:45:22 +0000

    first stage of implementing serveCandidates. Currently retrieves from the database all users of the opposite gender

--- server-config.js
+++ server-config.js
@@ -39,7 +39,7 @@ app.get('/auth/facebook/callback',
 
 
 app.get('/',  handler.renderIndex);
-app.get('/allcandidates', handle.serveCandidates);
+app.get('/allcandidates', handler.serveCandidates);
 app.get('/matches', iceAuthenticated, handler.serveMatches);
 app.post('/matches', iceAuthenticated, handler.postMatches);
 
