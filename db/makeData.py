a41d83fbe6a8ec0ef26dd10d4405626838af74a0
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-07-20 16:16:38 +0000

    import first name data and modify the script to use gender appropriate names

--- db/makeData.py
+++ db/makeData.py
@@ -1,18 +1,24 @@
 import random
 import json
 import time
+import csv
 
 
 ## fb_id | access_token | username | description | is_male | age | 
 
+
 def readFirstNames():
-  with open('./first_names.csv') as f:
-    content = f.readlines()
-    content = content[0].split('\r')
-
-  content = map(lambda name: name.strip(), content)
-  return content[1:]
-
+  males = []
+  females = []
+  with open('./yob2013.txt') as csvfile:
+    content = csv.reader(csvfile, delimiter=',')
+    for row in content:
+      if row[1] == 'M':
+        males.append(row[0])
+      else: 
+        females.append(row[0])
+  return (males, females)
+  
 
 def readLastNames():
   with open('./last_names.csv') as f:
@@ -22,39 +28,34 @@ def readLastNames():
   content = map(lambda name: name.strip(), content)
   return content[1:]
 
-
-
-def makeUser(name, names):
   
-  randomNumberOfMatches = random.randint(10,50);
-  user = {}
-  user['username'] = name
-  user['matches'] = [];
-  for i in range(randomNumberOfMatches):
-    randomMatch = random.randint(0, len(names)-1)
-    user['matches'].append(names[randomMatch])
-
-  return user
-
 def createData():
 
   count = 1
-  first_names = readFirstNames()
+  male_names, female_names = readFirstNames()
   last_names = readLastNames()
 
   with open("test.txt", "a") as myfile:
     for i in range(1000):
       result = []
-      randomIndex0 = random.randint(0, len(first_names))
-      randomIndex1 = random.randint(0, len(last_names))
-      first_name = first_names[randomIndex0]
+      is_male = random.randint(0,1)
+      first_name = ''      
+      
+      if is_male == 0:
+        randomIndex0 = random.randint(0, len(male_names))
+        first_name = male_names[randomIndex0]
+      else:
+        randomIndex0 = random.randint(0, len(female_names))
+        first_name = female_names[randomIndex0]
+
+      randomIndex1 = random.randint(0, len(female_names))                            
       last_name = last_names[randomIndex1]
       display_name = first_name + ' ' + last_name
       result.append(str(count))
       result.append('null_token')
       result.append(display_name)
       result.append('TODO Description')
-      result.append(str(random.randint(0,1)))
+      result.append(str(is_male))
       result.append(str(random.randint(18,30)))
       result.append(str(int(time.time())))
       result.append(str(int(time.time())))
@@ -62,3 +63,4 @@ def createData():
       count+=1
 
 createData()
+
